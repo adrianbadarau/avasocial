7d5e74db48322e293fdc00990991e1317bbb915d
adrianbadarau <adi.badarau@yahoo.com>
2016-05-14 17:07:08 +0300

    fb test

--- app/HelperClasses/FacebookBuilder.php
+++ app/HelperClasses/FacebookBuilder.php
@@ -21,14 +21,15 @@ class FacebookBuilder
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) return;
           js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=841918859275487";
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
     
         <!-- Your share button code -->
         <div class="fb-share-button" 
             data-href="{$link}" 
-            data-layout="{$type}">
+            data-layout="{$type}"
+            data-mobile-iframe="false">
         </div>
 HTML;
 
