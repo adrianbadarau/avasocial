57b7d02d2099bb4a922c0951ed8fa7297eea749f
adrianbadarau <adi.badarau@yahoo.com>
2016-05-14 13:43:46 +0300

    changed the user seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,20 +13,12 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'firstname'=>'admin',
-            'lastname'=>'babanu',
-            'email'=>'admin@site.com',
-            'password'=>bcrypt('secret'),
-            'code'=>'AVANTMD3',
-            'key'=>'7[42k6CMm*F3|8~1^l6]'
-        ]);
-        User::create([
-            'firstname'=>'root',
-            'lastname'=>'babanu',
-            'email'=>'root@site.com',
-            'password'=>bcrypt('secret'),
-            'code'=>'AVANTEST',
-            'key'=>'5&rl6N3%eT1O5(oW9~^I'
+            'firstname' => 'admin',
+            'lastname' => 'babanu',
+            'email' => 'admin@avasoci.al',
+            'password' => bcrypt('secret'),
+            'code' => 'AVGHACK2',
+            'key' => '9!R@4cU6|(R5?h69+5(2'
         ]);
     }
 }
